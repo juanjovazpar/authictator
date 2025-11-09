57bcb118dc5cd9e2ff7441b785a27f6588a6208a
Juanjo Vazquez <juanjovazpar@gmail.com>
2025-11-09 11:02:14 +0100

    Add users permissions for Redis

--- src/plugins/cache.plugin.ts
+++ src/plugins/cache.plugin.ts
@@ -3,11 +3,11 @@ import fp from 'fastify-plugin';
 import Redis from 'ioredis';
 
 export default fp(async function (fastify: FastifyInstance) {
-  // TODO: Define writer and reader users for Redis cache
   try {
     const redis: Redis = new Redis({
       host: process.env.REDIS_HOST,
       port: Number(process.env.REDIS_PORT),
+      username: process.env.REDIS_WRITER_USERNAME,
       password: process.env.REDIS_WRITER_PASSWORD,
     });
 
